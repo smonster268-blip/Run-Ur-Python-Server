2dc60c2180e5f606e9c378bb42352c1032eefea1
Old_Guy_19 <smonster268@gmail.com>
2026-03-09 08:57:24 +0530

    Update server_start.bat

--- server_start.bat
+++ server_start.bat
@@ -1,3 +1,12 @@
 @echo off
 cd /d "%~dp0"
 python -m http.server 8080
+
+' if u also want upload file 
+
+:: 1. Install a server that actually supports uploads
+pip install uploadserver
+
+:: 2. Start the server (standard port 8080)
+echo Starting server on http://localhost:8080
+python -m uploadserver 8080
